facc1de39ec4f8aa96c1def175c57d07f9ad473f
Dustin Coates <dustin@dcoates.com>
2016-12-22 16:14:00 +0100

    Add basic constructor

---
+++ src/index.js
@@ -0,0 +1,32 @@
+function algoliaAlexaAdapter (opts) {
+  if (!opts) {
+    throw new Error('Must initialize with options');
+  }
+
+  const {
+    algoliaAppId,
+    alexaAppId,
+    defaultIndexName,
+    comparisons,
+    availableSorts,
+    handlers,
+  } = opts;
+
+  const errorHandlers = {
+    algoliaError: undefined,
+  };
+
+  const skill = function() {};
+
+  skill.onAlgoliaError = function(e) {
+    return errorHandlers(e);
+  };
+
+  return skill;
+}
+
+algoliaAlexaAdapter.prototype.search = function(event, context) {
+  new this.Skill().execute(event, context);
+};
+
+module.exports = algoliaAlexaAdapter;
--- test/tap-test.js
+++ test/tap-test.js
@@ -1,6 +1,9 @@
 const test = require('tape');
+const algoliaAlexaAdapter = require('../src/index.js');
+const args = {};
 
-test('example test', t => {
-  t.plan(1);
-  t.equal(50 + 50, 100);
+test('constructor', t => {
+  t.plan(2);
+  t.equal(typeof algoliaAlexaAdapter(args), 'function', 'returns a function');
+  t.throws(algoliaAlexaAdapter, Error, 'throws with no arguments');
 });
