7255ba7a7910295dcbc1d7691769346c77624039
Dustin Coates <dustin@dcoates.com>
2017-08-02 16:40:39 +0200

    Use native isArray

--- src/utils/is_of_type.js
+++ src/utils/is_of_type.js
@@ -6,6 +6,6 @@ function isOfType(signature) {
 
 const isObject = isOfType('[object Object]');
 const isFunction = isOfType('[object Function]');
-const isArray = isOfType('[object Array]');
+const isArray = Array.isArray;
 
 export {isOfType, isObject, isFunction, isArray};
--- test/utils/is_of_type.test.js
+++ test/utils/is_of_type.test.js
@@ -2,7 +2,7 @@
   it, expect, describe
 */
 
-import {isOfType, isObject, isFunction} from '../../src/utils/is_of_type.js';
+import {isOfType, isObject, isFunction, isArray} from '../../src/utils/is_of_type.js';
 
 describe('isOfType', () => {
   it('returns a function', () => {
@@ -27,5 +27,15 @@ describe('isFunction', () => {
 
   it('return false if not a function', () => {
     expect(isFunction({})).toEqual(false);
+  });
+});
+
+describe('isArray', () => {
+  it('return true if a array', () => {
+    expect(isArray([])).toEqual(true);
+  });
+
+  it('return false if not a array', () => {
+    expect(isArray({})).toEqual(false);
   });
 });
